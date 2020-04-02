0e266546741dacfa24a1427d7adbdc40930cb510
thomasmburke <tburke@bu.edu>
2020-04-01 22:31:42 -0700

    added a button for testing purposes

---
+++ bruin-fitness/public/scripts/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// TODO: Enable firebase performance monitoring
+firebase.performance();
+
+var firestore = firebase.firestore();
+
+document.getElementById("clickMe").onclick = function () {
+    console.log('composite index query triggered...');
+    firestore.collection('workouts')
+    .where("name", "==", "tom")
+    .where("weight", "==", "200")
+    .where("age", ">", 20)
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc){
+            console.log(doc.id, " => ", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+};
