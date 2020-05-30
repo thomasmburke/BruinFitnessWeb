5506bac0c6bb7a25e8365f7d94e1bab16e0de1c1
thomasmburke <tburke@bu.edu>
2020-05-30 15:50:31 -0700

    adding clunky navbar -> to be improved

--- bruin-fitness/public/scripts/main.js
+++ bruin-fitness/public/scripts/main.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 // TODO: Enable firebase performance monitoring
 firebase.performance();
 
 var firestore = firebase.firestore();
-
+/*
 document.getElementById("clickMe").onclick = function () {
     console.log('composite index query triggered...');
     firestore.collection('workouts')
     .where("name", "==", "tom")
     .where("weight", "==", "200")
-    .where("age", ">", 20)
+    .where("age", "<", 30)
     .get()
     .then(function(querySnapshot) {
         querySnapshot.forEach(function(doc){
@@ -21,3 +21,4 @@ document.getElementById("clickMe").onclick = function () {
         console.log("Error getting documents: ", error);
     });
 };
+*/
