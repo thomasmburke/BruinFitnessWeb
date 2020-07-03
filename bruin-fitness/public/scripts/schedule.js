eb63b414be797212ef964a9753c4d1e41b08ec78
thomasmburke <tburke@bu.edu>
2020-07-02 18:20:06 -0700

    experimenting with creating a table with raw javascript

--- bruin-fitness/public/scripts/schedule.js
+++ bruin-fitness/public/scripts/schedule.js
@@ -16,17 +16,39 @@ function getRealtimeUpdates() {
 
 getRealtimeUpdates();
 
-// document.getElementById("clickMe").onclick = function () {
-//     console.log('Query triggered...');
-//     firestore.collection('schedules').document("San Leandro").collection("schedule").document("
-// LR7CLQA7fs5h07DAOpWb")
-//         .get()
-//         .then(function (querySnapshot) {
-//             querySnapshot.forEach(function (doc) {
-//                 console.log(doc.id, " => ", doc.data());
-//             });
-//         })
-//         .catch(function (error) {
-//             console.log("Error getting documents: ", error);
-//         });
-// };
+// DYNAMIC SCHEDULE TABLE CREATION
+
+let mountains = [
+  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+  { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+  { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Amiata", height: 1738, place: "Siena" },
+];
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (let x in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[x]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(mountains[0]);
+generateTable(table, mountains);
+generateTableHead(table, data);
