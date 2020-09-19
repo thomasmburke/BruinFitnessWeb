45976e85d14096a6b172d96b987072d82f021885
thomasmburke <tburke@bu.edu>
2020-09-19 12:45:48 -0700

    added padding

--- bruinfitness/src/components/about/AboutGoalPreface.jsx
+++ bruinfitness/src/components/about/AboutGoalPreface.jsx
@@ -10,21 +10,21 @@ const FadeInUpDiv = styled.div`
 
 function AboutGoalPreface() {
   return (
-    <FadeInUpDiv>
-      <div>
-        <h5 className="lead">
-          <strong>Our Goal:</strong>
-        </h5>
-        <p>
-          Bruin Fitness strives to build a community that is enthusiastic about
-          getting in shape. Our coaches work hard to provide programming to get
-          you in the best shape of your life. Come check out our gym to meet us
-          in person and see all the equipment at your disposal. Our gym offers a
-          mobile app to see what programming is coming for the week and cheer on
-          your gym buddies.
-        </p>
-      </div>
-    </FadeInUpDiv>
+    <div className="wrapper pt-3">
+      <FadeInUpDiv>
+        <div className="py-4">
+          <h5>Our Goal:</h5>
+          <p>
+            Bruin Fitness strives to build a community that is enthusiastic
+            about getting in shape. Our coaches work hard to provide programming
+            to get you in the best shape of your life. Come check out our gym to
+            meet us in person and see all the equipment at your disposal. Our
+            gym offers a mobile app to see what programming is coming for the
+            week and cheer on your gym buddies.
+          </p>
+        </div>
+      </FadeInUpDiv>
+    </div>
   );
 }
 
