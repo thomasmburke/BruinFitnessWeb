921ff76268b5e1a0e86721bb64f6babe7cde4d4f
thomasmburke <tburke@bu.edu>
2020-09-21 22:50:37 -0700

    finished callout box

--- bruinfitness/src/components/common/CalloutText.jsx
+++ bruinfitness/src/components/common/CalloutText.jsx
@@ -6,14 +6,14 @@ function CalloutText() {
   return (
     <React.Fragment>
       <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
-        <div className="row mx-5 mb-5 callout-border text-center">
-          <div className="col-md-8 col-lg-9 my-auto pb-0 pb-md-2">
+        <div className="flex-callout-box callout-border">
+          <div className="col-text">
             <h4 className="callout-text">
               Would you like to learn more about our gym and classes?
             </h4>
           </div>
 
-          <div className="col-md-4 col-lg-3 pb-2 pb-lg-1 py-1 vcenter align-center">
+          <div className="col-btn pb-2 pb-lg-1 py-1">
             <a className="btn btn-primary btn-md" href="/contact">
               Get In Touch
             </a>
@@ -25,3 +25,23 @@ function CalloutText() {
 }
 
 export default CalloutText;
+
+{
+  /* <React.Fragment>
+      <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
+        <div className="row mx-5 mb-5 callout-border text-center">
+          <div className="col-md-8 col-lg-9 my-auto pb-0 pb-md-2">
+            <h4 className="callout-text">
+              Would you like to learn more about our gym and classes?
+            </h4>
+          </div>
+
+          <div className="col-md-4 col-lg-3 pb-2 pb-lg-1 py-1 vcenter">
+            <a className="btn btn-primary btn-md" href="/contact">
+              Get In Touch
+            </a>
+          </div>
+        </div>
+      </ScrollAnimation>
+    </React.Fragment> */
+}
