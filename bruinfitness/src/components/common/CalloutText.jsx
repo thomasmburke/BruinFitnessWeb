36bcc603ec613dbf3939196a9ca7e922c8b56881
thomasmburke <tburke@bu.edu>
2020-09-22 21:51:23 -0700

    fixed blue highlight after nav dropdown item is clicked

--- bruinfitness/src/components/common/CalloutText.jsx
+++ bruinfitness/src/components/common/CalloutText.jsx
@@ -25,23 +25,3 @@ function CalloutText() {
 }
 
 export default CalloutText;
-
-{
-  /* <React.Fragment>
-      <ScrollAnimation animateIn="fadeInUp" animateOnce={true}>
-        <div className="row mx-5 mb-5 callout-border text-center">
-          <div className="col-md-8 col-lg-9 my-auto pb-0 pb-md-2">
-            <h4 className="callout-text">
-              Would you like to learn more about our gym and classes?
-            </h4>
-          </div>
-
-          <div className="col-md-4 col-lg-3 pb-2 pb-lg-1 py-1 vcenter">
-            <a className="btn btn-primary btn-md" href="/contact">
-              Get In Touch
-            </a>
-          </div>
-        </div>
-      </ScrollAnimation>
-    </React.Fragment> */
-}
