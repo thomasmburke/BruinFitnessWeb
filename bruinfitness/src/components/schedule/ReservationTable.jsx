8ce33aaeee112817f42747ca5cc7dca6560b3a06
thomasmburke <tburke@bu.edu>
2020-12-16 23:09:25 -0500

    check to see if user is in list of reservedUser and display reservation options accordingly

--- bruinfitness/src/components/schedule/ReservationTable.jsx
+++ bruinfitness/src/components/schedule/ReservationTable.jsx
@@ -1,140 +1,183 @@
-import React, { useState, useContext, useEffect } from 'react'
+import React, { useState, useContext, useEffect } from "react";
 import { MyContext } from "../../providers/MyProvider";
 import { firestore } from "../../firebase";
 import "./ReservationTable.css";
 
 // Get a reference to the schedule collection of interest
 // TODO: This should get today's date and use that in the collection path
-const reservationRef = firestore.collection("schedules/Redwood City/dates/2020_12_13/classes");
+const reservationRef = firestore.collection(
+  "schedules/Redwood City/dates/2020_12_13/classes"
+);
+const testUser = "tom";
 
 function ReservationTable() {
-    const context = useContext(MyContext)
-    const [reservationData, setReservationData] = useState(null);
-    const headers = ["Time", "Workout Type"];
-    // On function component mount lifecycle action
-    useEffect(() => {
+  const context = useContext(MyContext);
+  const [reservationData, setReservationData] = useState(null);
+  const headers = ["Time", "Workout Type"];
+  // On function component mount lifecycle action
+  useEffect(() => {
     /**
      * Summary: Reach out to Firestore and get all schedule/reservation entry data
      * @return {Object} Object containing multiple schedule entries
      */
-        var unsubscribe = getReservationData();
-        // remember to unsubscribe from your realtime listener on unmount or you will create a memory leak. Why did we return a function from our effect? This is the optional cleanup mechanism for effects.
-       return () => unsubscribe();
-    }, []);
+    var unsubscribe = getReservationData();
+    // remember to unsubscribe from your realtime listener on unmount or you will create a memory leak. Why did we return a function from our effect? This is the optional cleanup mechanism for effects.
+    return () => unsubscribe();
+  }, []);
 
-     // code to getSchedule using a firestore listener
-    function getReservationData() {
-        // if using a listener you will also need to detach the listener
-      try {
-        var unsubscribe = reservationRef.onSnapshot(function(querySnapshot) {
-            let reservationData = [];
-             // querySnapshot holds multiple documents, we need to unpack all of them
+  // code to getSchedule using a firestore listener
+  function getReservationData() {
+    // if using a listener you will also need to detach the listener
+    try {
+      var unsubscribe = reservationRef.onSnapshot(function (querySnapshot) {
+        let reservationData = [];
+        // querySnapshot holds multiple documents, we need to unpack all of them
         querySnapshot.forEach(function (doc) {
           let docData = doc.data();
           // for each workout listed in the firestore document create a schedule entry
-        reservationData.push({
+          reservationData.push({
             "Workout Type": docData.workoutType,
             id: doc.id,
-            "reservationCnt": docData.reservationCnt,
-            Time: docData.time
-        })
+            reservationCnt: docData.reservationCnt,
+            Time: docData.time,
+            reservedUsers: docData.reservedUsers,
+          });
         });
         setReservationData(reservationData);
-        });
-        return unsubscribe;
-      } catch (err) {
-        console.log("Error getting documents", err);
-      }
+      });
+      return unsubscribe;
+    } catch (err) {
+      console.log("Error getting documents", err);
     }
-
-    function incrementReservation(docId){
-    //   get a reference to the doc being updated
-      let docRef = reservationRef.doc(docId)
-      // using a transaction to ensure we don't exceed the max number of reservations
-      return firestore.runTransaction(function(transaction) {
-        // This code may get re-run multiple times if there are conflicts.
-        return transaction.get(docRef).then(function(reservationDoc) {
-            if (!reservationDoc.exists) {
-                throw "Document does not exist!";
-            }
-            // Get the current reservation count
-            let reservationCnt = reservationDoc.data().reservationCnt;
-            if (reservationCnt >= 12){
-                //TODO: should likely put a toast or alert that they were unable to register for the class
-                console.log("sorry class is full!")
-            } else {
-                // if there is still space left in the class then add the user and increment the count
-                //TODO: add the user's id to the list of reserved users
-                reservationCnt++;
-                transaction.update(docRef, { reservationCnt: reservationCnt });
-            }
-        });
-    }).then(function() {
-        console.log("Transaction successfully committed!");
-    }).catch(function(error) {
-        console.log("Transaction failed: ", error);
-    });
   }
 
-    return (
-        <div>
-            <table className="table table-bordered table-hover table-sm">
-                <thead className="thead-dark">
-                    <tr>
-                    <th colSpan="3">{context.state.scheduleDate} Classes</th>
-                    </tr>
-                </thead>
-                <TableBody headers={headers} rows={reservationData} incrementReservation={incrementReservation}></TableBody>
-            </table>
-        </div>
-    )
+  function removeReservation(row) {
+    console.log(`removing reservation for ${testUser}`);
+  }
+
+  function incrementReservation(docId) {
+    //   get a reference to the doc being updated
+    let docRef = reservationRef.doc(docId);
+    // using a transaction to ensure we don't exceed the max number of reservations
+    return firestore
+      .runTransaction(function (transaction) {
+        // This code may get re-run multiple times if there are conflicts.
+        return transaction.get(docRef).then(function (reservationDoc) {
+          if (!reservationDoc.exists) {
+            throw "Document does not exist!";
+          }
+          // Get the current reservation count
+          let reservationCnt = reservationDoc.data().reservationCnt;
+          if (reservationCnt >= 12) {
+            //TODO: should likely put a toast or alert that they were unable to register for the class
+            console.log("sorry class is full!");
+          } else {
+            // if there is still space left in the class then add the user and increment the count
+            //TODO: add the user's id to the list of reserved users
+            reservationCnt++;
+            transaction.update(docRef, { reservationCnt: reservationCnt });
+          }
+        });
+      })
+      .then(function () {
+        console.log("Transaction successfully committed!");
+      })
+      .catch(function (error) {
+        console.log("Transaction failed: ", error);
+      });
+  }
+
+  return (
+    <div>
+      <table className="table table-bordered table-hover table-sm">
+        <thead className="thead-dark">
+          <tr>
+            <th colSpan="3">{context.state.scheduleDate} Classes</th>
+          </tr>
+        </thead>
+        <TableBody
+          headers={headers}
+          rows={reservationData}
+          incrementReservation={incrementReservation}
+          removeReservation={removeReservation}
+        ></TableBody>
+      </table>
+    </div>
+  );
 }
 
-const TableBody = ({ headers, rows, incrementReservation }) => {
+const TableBody = ({
+  headers,
+  rows,
+  incrementReservation,
+  removeReservation,
+}) => {
   const columns = headers ? headers.length : 0;
   const showSpinner = rows === null;
+
+  function buildReservationButton(row) {
+    // If the user has reserved a spot in the class
+    if (row["reservedUsers"].includes(testUser)) {
+      // If they are in the class we need to allow them to leave the class if they can't make it
+      return (
+        <div className="col-md-3">
+          <button
+            className="btn btn-primary btn-sm"
+            onClick={() => removeReservation(row)}
+          >
+            Leave Class
+          </button>
+          <br />
+          <small>{row["reservationCnt"]} of 12 reserved</small>
+        </div>
+      );
+    }
+    // If the user has not reserved a spot in the class
+    else {
+      if (row["reservationCnt"] < 12) {
+        // If they are not in the class and there are still spaces available then prompt them with a button to reserve a spot
+        return (
+          <div className="col-md-3">
+            <button
+              className="btn btn-primary btn-sm"
+              onClick={() => incrementReservation(row["id"])}
+            >
+              Reserve
+            </button>
+            <br />
+            <small>{row["reservationCnt"]} of 12 reserved</small>
+          </div>
+        );
+      } else {
+        return (
+          // If they are not in the class and the class is full we remove the button and mark it as full
+          <div className="col-md-3">Class Full</div>
+        );
+      }
+    }
+  }
 
   function buildRow(row, headers) {
     return (
       // Wonky way of creating a unique key per row, but works for now
       <tr key={row["Workout Type"] + row["Time"]}>
         {headers.map((value, index) => {
-            return <td key={index}>
-                {/* if it is the column with workout type, then we need to render resevation btn etc */}
-                {value === "Workout Type" &&
-                        (
-                            <div className="container">
-                                <div className="row">
-                                    <div className="col-md-9 vcenter">
-                                        {row[value]}
-                                    </div>
-                                    {/* If less than 12 people have registered we need present a reservation button and how many spots are left */}
-                                    {row["reservationCnt"] < 12 ?
-                                        (
-                                            <div className="col-md-3">
-                                                <button className="btn btn-primary btn-sm"
-                                                onClick={() => incrementReservation(row["id"])}>Reserve</button>
-                                                <br />
-                                                <small>{row["reservationCnt"]} of 12 reserved</small>
-                                            </div>
-                                        ) : (
-                                            // otherwise we need to mark the class as full
-                                            <div className="col-md-3">
-                                                Class Full
-                                            </div>
-                                        )
-                                    }
-
-                                </div>
-                            </div>
-                        )}
-                {value !== "Workout Type"&&
-                    (
-                        <React.Fragment>
-                        {row[value]}
-                        </React.Fragment>
-                    )}
-            </td>;
+          return (
+            <td key={index}>
+              {/* if it is the column with workout type, then we need to render resevation btn etc */}
+              {value === "Workout Type" && (
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-9 vcenter">{row[value]}</div>
+                    {buildReservationButton(row)}
+                  </div>
+                </div>
+              )}
+              {value !== "Workout Type" && (
+                <React.Fragment>{row[value]}</React.Fragment>
+              )}
+            </td>
+          );
         })}
       </tr>
     );
@@ -160,4 +203,4 @@ const TableBody = ({ headers, rows, incrementReservation }) => {
   );
 };
 
-export default ReservationTable
+export default ReservationTable;
