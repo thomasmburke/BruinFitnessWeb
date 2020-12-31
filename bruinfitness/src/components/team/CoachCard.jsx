21d076050070e526e83d4190bf6cdf0ae8bf890e
thomasmburke <tburke@bu.edu>
2020-12-31 10:58:50 -0500

    added coach cards, some styling still needed

--- bruinfitness/src/components/team/CoachCard.jsx
+++ bruinfitness/src/components/team/CoachCard.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
+import ScrollAnimation from "react-animate-on-scroll";
 
-function CoachCard() {
+function CoachCard({animation, coachImgPath, coachName, coachDesc}) {
     return (
-        <div>
-            
-        </div>
+        <div className="col-md-4">
+            <ScrollAnimation animateIn={animation} animateOnce={true}>
+              <div className="card">
+                <img className="card-img-top" src={coachImgPath} alt=""/>
+                <div className="card-body">
+                  <h4 className="card-title">{coachName}</h4>
+                  <hr />
+                  <p className="card-text">{coachDesc}</p>
+                  <a href="#" className="btn btn-outline-secondary">See Profile</a>
+                </div>
+              </div>
+              </ScrollAnimation> 
+          </div>
     )
 }
 
--- bruinfitness/src/components/team/Team.jsx
+++ bruinfitness/src/components/team/Team.jsx
@@ -1,12 +1,36 @@
 import React from 'react'
 import CalloutText from '../common/CalloutText'
 import WebPageHeader from '../common/WebPageHeader'
+import CoachCard from './CoachCard'
 
 function Team() {
     return (
         <div className="wrapper pt-2">
             <WebPageHeader header="Our Team" />
+            <div className="container-fluid padding">
+                <div className="row padding">
+                    <CoachCard 
+                    animation="slideInLeft" 
+                    coachImgPath="/images/tomburke.jpg" 
+                    coachName="Tom Burke" 
+                    coachDesc="Tom is an athelete from several backgrounds including kung
+                            fu, weightlifting, and gymnastics"/>
+                    
+                    <CoachCard 
+                    animation="slideInUp" 
+                    coachImgPath="/images/margauxcarle.jpg" 
+                    coachName="Margaux Carle" 
+                    coachDesc="Margaux is the head CrossFit coach also boasting an
+                    impressive background in nutrition"/>
 
+                    <CoachCard 
+                    animation="slideInRight" 
+                    coachImgPath="/images/tomburke.jpg" 
+                    coachName="Tom Again" 
+                    coachDesc="Tom is an athelete from several backgrounds including kung
+                            fu, weightlifting, and gymnastics"/>
+                </div>
+            </div>
             <CalloutText />
         </div>
     )
