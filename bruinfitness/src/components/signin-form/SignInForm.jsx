48e02b85b0960d4003012f40e18fa4cf65e0a8cb
thomasmburke <tburke@bu.edu>
2020-12-30 20:53:06 -0500

    making the signup form look nicer

--- bruinfitness/src/components/signin-form/SignInForm.jsx
+++ bruinfitness/src/components/signin-form/SignInForm.jsx
@@ -1,7 +1,9 @@
 import 'firebase/auth';
 import React from 'react';
+import Card from "react-bootstrap/Card";
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import { useAuth } from 'reactfire';
+import "./SignInForm.css";
 
 const SignInForm = () => {
     const auth = useAuth;
@@ -9,19 +11,35 @@ const SignInForm = () => {
     const uiConfig = {
       signInFlow: 'popup',
       signInOptions: [
-          auth.GoogleAuthProvider.PROVIDER_ID,
-          auth.EmailAuthProvider.PROVIDER_ID],
-          // Redirect to / (the home page) after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-          signInSuccessUrl: '/',
+        {
+          provider: auth.EmailAuthProvider.PROVIDER_ID,
+          // Whether the displayName should be displayed in the sign up page
+          requireDisplayName: false
+        },
+        auth.GoogleAuthProvider.PROVIDER_ID
+        ],
+        // Redirect to / (the home page) after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+        signInSuccessUrl: '/',
     //   callbacks: {
     //     // Avoid redirects after sign-in.
     //     signInSuccessWithAuthResult: () => false
     //   }
     };
-  
     return (
-        <div>
-            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+      <div className="fullpage">
+        <div className="wrapper">
+          <Card bg="dark" text="light" border="dark" className="text-center">
+            {/* <Card.Header>Member Sign In</Card.Header> */}
+            <Card.Body>
+              <Card.Title>Member Sign In</Card.Title>
+              <Card.Text>
+                Only members can sign in, to get an account come visit our gym!
+              </Card.Text>
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={auth()} />
+            </Card.Body>
+            {/* <Card.Footer className="text-muted"></Card.Footer> */}
+          </Card>  
+        </div>
         </div>
     );
   };
