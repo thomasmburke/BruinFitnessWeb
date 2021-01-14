6e892c19a5a42bb975c10c183eec009b4d198450
thomasmburke <tburke@bu.edu>
2021-01-13 22:17:21 -0800

    writing to Firestore with doc(workoutDate).set() and merge: true so pre-existing docs can be edited

--- bruinfitness/src/components/admin/Admin.jsx
+++ bruinfitness/src/components/admin/Admin.jsx
@@ -6,28 +6,36 @@ import { useFirestore } from "reactfire";
 
 function Admin() {
 
-    // equivalent of firebase.firestore(), but making use of React Context API to ensure it is a singleton
-    const firestore = useFirestore();
     const [workoutDate, setWorkoutDate] = useState(new Date().toISOString().substring(0, 10));
     const [workoutType, setWorkoutType] = useState('Metcon');
     const [warmUp, setWarmUp] = useState();
     const [skill, setSkill] = useState();
     const [strength, setStrength] = useState();
+    const [coolDown, setCoolDown] = useState();
     const [workout, setWorkout] = useState();
+
+    // equivalent of firebase.firestore(), but making use of React Context API to ensure it is a singleton
+    const firestore = useFirestore();
+    const workoutRef = firestore.collection(
+        "workouts"
+      ).doc(workoutDate);
 
     const handleSubmit = (event) => {
         event.preventDefault();
         // If you want to see the \n newline characters
         // console.log(JSON.stringify(`workoutType: ${workoutType}\nwarmUp: ${warmUp}\nstrength: ${strength}\nworkout: ${workout}`));
-        console.log(`workoutDate: ${workoutDate}\nworkoutType: ${workoutType}\nwarmUp: ${warmUp}\nskill: ${skill}\nstrength: ${strength}\nworkout: ${workout}`);
-        firestore.collection('workouts').add({
-            workoutType: workoutType,
-            warmUp: warmUp || null,
-            skill: skill || null,
-            strength: strength || null,
-            workout: workout,
-            workoutDate: workoutDate
-        })
+        console.log(`workoutDate: ${workoutDate}\nworkoutType: ${workoutType}\nwarmUp: ${warmUp}\nskill: ${skill}\nstrength: ${strength}\ncool down: ${coolDown}\nworkout: ${workout}`);
+        firestore.collection('workouts').doc(workoutDate).set({
+            [workoutType]: {
+                workoutType: workoutType,
+                warmUp: warmUp || null,
+                skill: skill || null,
+                strength: strength || null,
+                coolDown: coolDown || null,
+                workout: workout,
+                workoutDate: workoutDate
+            }
+        }, {merge: true})
         .then(() => console.log("workout successfully added!"))
         .catch((error) => console.log(`Error writing workout document: ${error}`))
     }
@@ -67,11 +75,15 @@ function Admin() {
                         <Form.Label>STRENGTH</Form.Label>
                         <Form.Control as="textarea" rows={12} value={strength} onChange={e => setStrength(e.target.value)}/>
                     </Form.Group>
-                    <Form.Group controlId="workout" as={Col}>
-                        <Form.Label>WORKOUT</Form.Label>
-                        <Form.Control as="textarea" rows={12} required value={workout} onChange={e => setWorkout(e.target.value)}/>
+                    <Form.Group controlId="coolDown" as={Col}>
+                        <Form.Label>COOL DOWN</Form.Label>
+                        <Form.Control as="textarea" rows={12} value={coolDown} onChange={e => setCoolDown(e.target.value)}/>
                     </Form.Group>
                 </Form.Row>
+                <Form.Group controlId="workout" as={Col}>
+                        <Form.Label>WORKOUT</Form.Label>
+                        <Form.Control as="textarea" rows={12} required value={workout} onChange={e => setWorkout(e.target.value)}/>
+                </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
                 </Button>
