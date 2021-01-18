d9260415e260a852e4aeb5b8ecb619e691449a40
thomasmburke <tburke@bu.edu>
2021-01-17 21:59:33 -0800

    simple db read and publish POC

--- bruinfitness/src/components/schedule/WorkoutTabs.jsx
+++ bruinfitness/src/components/schedule/WorkoutTabs.jsx
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
+import { useFirestore } from "reactfire";
+import { MyContext } from "../../providers/MyProvider";
 import "./WorkoutTabs.css";
 
 function WorkoutTabs() {
+    // equivalent of firebase.firestore(), but making use of React Context API to ensure it is a singleton
+    const firestore = useFirestore();
+    // This context object holds the state from the DatePicker component which sets the date
+    const context = useContext(MyContext);
+
+    // Mainly using useRef here so eslint doesn't make me put emptyWorkoutInfo as a trigger arg for useEffect
+    const emptyWorkoutInfo = useRef({warmUp: '', skill: '', strength: '', coolDown: '', workout: ''});
+    const prevWorkoutType = useRef('Metcon');
+    const workoutData = useRef()
+
+    // Set to today's date in YYYY-mm-DD string format
+    const [workoutDate, setWorkoutDate] = useState(new Date().toISOString().substring(0, 10));
+    // Metcon is currently the first option in the selector so it is the default
+    const [workoutType, setWorkoutType] = useState('Metcon');
+    // Holds the meat of the workout programming
+    const [workoutInfo, setWorkoutInfo] = useState(emptyWorkoutInfo.current)
+
+    useEffect(() => {
+        firestore.collection("workouts").doc('2021-01-15').get().then(function(doc) {
+            if (doc.exists) {
+                workoutData.current = doc.data();
+                console.log(JSON.stringify(workoutData.current, null, 2))
+                // Check if we previously recorded a workout for the selected workoutType
+                if (workoutData.current[workoutType]) {
+                    console.log('made it here')
+                    // If there is a previously logged workout for the workoutType update the workoutInfo state object
+                    setWorkoutInfo(workoutData.current[workoutType])
+                }
+                else {
+                    // If we have not previously logged a workout for the selected workoutType
+                    // then set the workoutInfo state object to empty
+                    setWorkoutInfo(emptyWorkoutInfo.current);
+                }
+            }
+            else {
+                // If the doc we are looking for does not exist then workoutInfo should be empty
+                console.log(`no workout document`)
+                // setWorkoutInfo(emptyWorkoutInfo.current);
+                // workoutData.current = emptyWorkoutInfo.current;
+            }
+        }).catch(function(error) {
+            console.log(`Error getting document: ${error}`)
+        });
+    }, [firestore, workoutType])
+
     const text = "WORKOUT\n4 SETS FOR QUALITY\n3/3DB Turkish Get-Ups\n12/12 Tempo Supported DB Row\n1:00 DB Front Rack Hold\n30 Weighted Sit-Ups"
+
     return (
         <div>
             <Tab.Container id="left-tabs-example" defaultActiveKey="first">
@@ -30,7 +78,11 @@ function WorkoutTabs() {
                             <div style={{whiteSpace: "pre-wrap"}}>
                                 {/* may want pre-line instead */}
                                 {/* make text section a fixed size for certain screen sizes and potentially make it scrollable */}
-                                <p>{text}</p>
+                                {/* <p>{text}</p> */}
+                                {/* <p>WORKOUT</p> */}
+                                {console.log(JSON.stringify(workoutData.current, null, 2))}
+                                {workoutData.current !== undefined && workoutData.current['Metcon'] !== undefined && (<p>{workoutData.current['Metcon'].workout}</p>)}
+                                {/* <p>{workoutData !== undefined && workoutData['Metcon'] !== undefined && workoutData['Metcon'].workout}</p> */}
                             </div>
                         </Tab.Pane>
                         <Tab.Pane eventKey="second">
