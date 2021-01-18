a51af7cc6ff896d7bc37c7cf37256428ffc1f2d3
thomasmburke <tburke@bu.edu>
2021-01-17 16:18:36 -0800

    added Roboto font

--- bruinfitness/src/index.jsx
+++ bruinfitness/src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { FirebaseAppProvider } from "reactfire";
+import 'typeface-roboto';
 import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
