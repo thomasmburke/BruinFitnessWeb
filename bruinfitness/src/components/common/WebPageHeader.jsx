59bb7835064ee5ad0a81e6ddef586dd9b6becb3a
thomasmburke <tburke@bu.edu>
2021-01-31 17:31:56 -0800

    component now can be passed a custom className

--- bruinfitness/src/components/common/WebPageHeader.jsx
+++ bruinfitness/src/components/common/WebPageHeader.jsx
@@ -8,10 +8,10 @@ const FadeInUpDiv = styled.div`
   animation: 1s ${fadeInUpAnimation} !important;
 `;
 
-function WebPageHeader({header}) {
+function WebPageHeader({header, additionalClassNames}) {
     return (
         <FadeInUpDiv style={{paddingBottom: "1rem", paddingTop: "1rem"}}>
-          <h1 className="pageHeader">{header}</h1>
+          <h1 className={`pageHeader ${additionalClassNames}`}>{header}</h1>
         </FadeInUpDiv>
     )
 }
