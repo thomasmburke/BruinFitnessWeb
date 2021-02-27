a07ef6da1a0929cb26b319704445eee58f7e00e6
thomasmburke <tburke@bu.edu>
2021-02-26 21:15:29 -0800

    made FAQ accordion context aware

--- bruinfitness/src/components/faq/FAQ.jsx
+++ bruinfitness/src/components/faq/FAQ.jsx
@@ -1,5 +1,9 @@
-import React from 'react';
+import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useContext } from 'react';
 import Accordion from "react-bootstrap/Accordion";
+import AccordionContext from "react-bootstrap/AccordionContext";
+import { useAccordionToggle } from 'react-bootstrap/AccordionToggle';
 import Card from "react-bootstrap/Card";
 import CalloutText from '../common/CalloutText';
 import WebPageHeader from '../common/WebPageHeader';
@@ -8,29 +12,42 @@ import "./FAQ.css";
 function FAQ() {
     return (
             <div className="wrapper pt-2">
-                <WebPageHeader header="FAQ"/>
+                <WebPageHeader header="FREQUENTLY ASKED QUESTIONS"/>
                 <FAQAccordion />
                 <CalloutText />
             </div>
     )
 }
 
+function AccordionToggle({eventKey, faqQuestion, callback}) {
+    const currentEventKey = useContext(AccordionContext);
+    const onClickToggle = useAccordionToggle(
+        eventKey,
+        () => callback && callback(eventKey),
+      );
+    const isCurrentEventKey = currentEventKey === eventKey;
+    return (
+        <React.Fragment>
+            <Accordion.Toggle as={Card.Header} eventKey={eventKey} onClick={onClickToggle} style={{whiteSpace: "pre-wrap"}} className="py-3">
+                { isCurrentEventKey ? <FontAwesomeIcon icon={faMinus} /> : <FontAwesomeIcon icon={faPlus} /> }{`\t${faqQuestion}`}
+            </Accordion.Toggle>
+        </React.Fragment>
+    )
+}
+
+
 function FAQAccordion() {
     return (
         <React.Fragment>
-            <Accordion defaultActiveKey="0">
+            <Accordion>
                 <Card className="card-no-shadow text-white">
-                    <Accordion.Toggle as={Card.Header} eventKey="0">
-                    What does a membership include
-                    </Accordion.Toggle>
+                    <AccordionToggle eventKey="0" faqQuestion="What does a membership include?"/>
                     <Accordion.Collapse eventKey="0">
                     <Card.Body className="bg-dark">You get an all access pass to weights and gains</Card.Body>
                     </Accordion.Collapse>
                 </Card>
                 <Card className="card-no-shadow text-white">
-                    <Accordion.Toggle as={Card.Header} eventKey="1">
-                    What if I don't know how to do the movements
-                    </Accordion.Toggle>
+                    <AccordionToggle eventKey="1" faqQuestion="What if I don't know how to do the movements?"/>
                     <Accordion.Collapse eventKey="1">
                         <Card.Body  className="bg-dark">
                             Our coaches are equip to get you up to speed before you know it. 
@@ -40,9 +57,7 @@ function FAQAccordion() {
                     </Accordion.Collapse>
                 </Card>
                 <Card className="card-no-shadow text-white">
-                    <Accordion.Toggle as={Card.Header} eventKey="2">
-                    What if I don't know how to do the movements
-                    </Accordion.Toggle>
+                <AccordionToggle eventKey="2" faqQuestion="What if I don't know how to do the movements?"/>
                     <Accordion.Collapse eventKey="2">
                         <Card.Body  className="bg-dark">
                             Our coaches are equip to get you up to speed before you know it. 
